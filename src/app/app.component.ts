14126579f89aebb3179f8238924f82c89fd335a4
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-21 17:16:33 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {
   ReactiveFormsModule,
   FormControl,
-  FormGroup
+  FormGroup,
+  Validators
 } from "@angular/forms";
 
 @Component({
@@ -14,9 +15,18 @@ import {
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   form = new FormGroup({
-
+    login: new FormControl('', Validators.required), email: new FormControl(
+      '', [Validators.required, Validators.email]
+    ),
+    password: new FormControl('')
   })
+
+  handle() {
+    console.log(this.form.get(['login'])!.value)
+  }
+
+  ngOnInit(): void { }
 
 }
